da3e9d6bda1b1b12af261f4c0ac5fc784fbc69fb
terry <terrymagnusdrever@wellfired.com>
2015-12-20 20:39:22 +0900

    Add a simple native renderer for our window control

---
+++ Editor/NativeControls/BaseRenderer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using WellFired.Guacamole;
+
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public class BaseRenderer : INativeRenderer
+	{
+		#region INativeRenderer implementation
+		public ViewBase Control 
+		{
+			get;
+			set;
+		}
+		#endregion
+	}
+}
---
+++ Editor/NativeControls/WindowRenderer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+using WellFired.Guacamole;
+
+[assembly: CustomRenderer (typeof(Window), typeof(WellFired.Guacamole.Unity.Editor.WindowRenderer))]
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public class WindowRenderer : BaseRenderer
+	{
+		
+	}
+}
