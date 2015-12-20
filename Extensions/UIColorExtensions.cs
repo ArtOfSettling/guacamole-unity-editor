ce29092426ef090ab79a5626df16dd7352402aca
terry <terrymagnusdrever@wellfired.com>
2015-12-20 17:36:16 +0900

    Added some colour extensions, converting between Unity Color and guacamole colour.

---
+++ Extensions/UIColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using WellFired.Guacamole;
+
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public static class UIColorExtensions 
+	{
+		public static UnityEngine.Color ToUnityRect(this UIColor source)
+		{
+			return new UnityEngine.Color (source.R, source.G, source.B);
+		}
+	}
+}
---
+++ Extensions/UnityColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using WellFired.Guacamole;
+
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public static class UnityColorExtensions 
+	{
+		public static UIColor ToUISize(this UnityEngine.Color source)
+		{
+			return UIColor.FromRGB ((int)(source.r * 255.0f), (int)(source.g * 255.0f), (int)(source.b * 255.0f));
+		}
+	}
+}
