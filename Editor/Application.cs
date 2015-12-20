2c25aee78d5b4d9b7a9c3d6b51e8df98092ecb94
terry <terrymagnusdrever@wellfired.com>
2015-12-20 18:32:56 +0900

    A wrapper for Unity's window, preserving the Application / Window split.

--- Editor/Application.cs
+++ Editor/Application.cs
@@ -1,61 +1,19 @@
 ﻿using UnityEditor;
+using WellFired.Guacamole;
 
 namespace WellFired.Guacamole.Unity.Editor
 {
-	public class Application : UnityEditor.EditorWindow, IApplication
+	public class Application : WellFired.Guacamole.IApplication
 	{
-		public ViewBase MainContent
+		public IWindow MainWindow 
 		{
 			get;
 			set;
 		}
 
-		public UIRect Rect 
+		public void Launch()
 		{
-			get 
-			{
-				return this.position.ToUIRect ();
-			}
-			set 
-			{
-				this.position = value.ToUnityRect ();
-			}
-		}
-
-		public UISize MinSize 
-		{
-			get 
-			{
-				return this.minSize.ToUISize ();
-			}
-			set 
-			{
-				this.minSize = value.ToUnityVector2 ();
-			}
-		}
-
-		public UISize MaxSize 
-		{
-			get 
-			{
-				return this.maxSize.ToUISize ();
-			}
-			set 
-			{
-				this.maxSize = value.ToUnityVector2 ();
-			}
-		}
-
-		public static IApplication Launch()
-		{
-			var application = EditorWindow.GetWindow<Application>();
-			application.Show();
-			return application;
-		}
-
-		public void OnGUI()
-		{
-
+			MainWindow = EditorWindow.GetWindow<GuacamoleWindow>();
 		}
 	}
 }
---
+++ Editor/GuacamoleWindow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public class GuacamoleWindow : EditorWindow, IWindow
+	{
+		public Window MainContent
+		{
+			get;
+			set;
+		}
+
+		public string Title
+		{
+			get 
+			{
+				return this.titleContent.text;
+			}
+			set 
+			{
+				this.titleContent.text = value;
+			}
+		}
+
+		public UIRect Rect 
+		{
+			get 
+			{
+				return this.position.ToUIRect ();
+			}
+			set 
+			{
+				this.position = value.ToUnityRect ();
+			}
+		}
+
+		public UISize MinSize 
+		{
+			get 
+			{
+				return this.minSize.ToUISize ();
+			}
+			set 
+			{
+				this.minSize = value.ToUnityVector2 ();
+			}
+		}
+
+		public UISize MaxSize 
+		{
+			get 
+			{
+				return this.maxSize.ToUISize ();
+			}
+			set 
+			{
+				this.maxSize = value.ToUnityVector2 ();
+			}
+		}
+
+		public void OnGUI()
+		{
+			if(Event.current.type == EventType.Layout)
+				MainContent.Layout ();
+			if(Event.current.type == EventType.Repaint)
+				MainContent.Render ();
+		}
+	}
+}
