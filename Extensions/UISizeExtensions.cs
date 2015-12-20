e601376ed01b5af0317023910ccec05dd58caa76
terry <terrymagnusdrever@wellfired.com>
2015-12-20 17:30:26 +0900

    Extensions converting between Vector2 and UISize.

---
+++ Extensions/UISizeExtensions.cs
@@ -0,0 +1,9 @@
+﻿using WellFired.Guacamole;
+
+public static class UISizeExtensions 
+{
+	public static UnityEngine.Vector2 ToUnityVector2(this UISize source)
+	{
+		return new UnityEngine.Vector2 (source.Width, source.Height);
+	}
+}
---
+++ Extensions/UnityVector2Extensions.cs
@@ -0,0 +1,9 @@
+﻿using WellFired.Guacamole;
+
+public static class UnityVector2Extensions 
+{
+	public static UISize ToUISize(this UnityEngine.Vector2 source)
+	{
+		return new UISize ((int)source.x, (int)source.y);
+	}
+}
