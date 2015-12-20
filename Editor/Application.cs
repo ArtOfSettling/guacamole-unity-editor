954e516590f2499bb210bd39ba714cbb7f53e35a
terry <terrymagnusdrever@wellfired.com>
2015-12-20 17:29:40 +0900

    Simple Wrapper for Unity Window.

---
+++ Editor/Application.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public class Application : UnityEditor.EditorWindow, IApplication
+	{
+		public ViewBase MainContent
+		{
+			get;
+			set;
+		}
+
+		public UIRect Rect 
+		{
+			get 
+			{
+				return this.position.ToUIRect ();
+			}
+			set 
+			{
+				this.position = value.ToUnityRect ();
+			}
+		}
+
+		public UISize MinSize 
+		{
+			get 
+			{
+				return this.minSize.ToUISize ();
+			}
+			set 
+			{
+				this.minSize = value.ToUnityVector2 ();
+			}
+		}
+
+		public UISize MaxSize 
+		{
+			get 
+			{
+				return this.maxSize.ToUISize ();
+			}
+			set 
+			{
+				this.maxSize = value.ToUnityVector2 ();
+			}
+		}
+
+		public static IApplication Launch()
+		{
+			var application = EditorWindow.GetWindow<Application>();
+			application.Show();
+			return application;
+		}
+
+		public void OnGUI()
+		{
+
+		}
+	}
+}
