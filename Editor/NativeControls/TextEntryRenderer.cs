7fbc077b8412caab231d84baee28dc3a55aa21f8
Terry Drever <terrymagnusdrever@wellfired.com>
2016-01-31 20:33:12 +0900

    Add really basic support for number and text entry.

---
+++ Editor/NativeControls/TextEntryRenderer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using WellFired.Guacamole;
+
+[assembly : CustomRenderer(typeof(TextEntry), typeof(WellFired.Guacamole.Unity.Editor.TextEntryRenderer))]
+namespace WellFired.Guacamole.Unity.Editor
+{
+	public class TextEntryRenderer : BaseRenderer
+	{
+		public override void Render(UIRect renderRect)
+		{
+			var entry = Control as Entry;
+			entry.Text = UnityEditor.EditorGUI.TextField(renderRect.ToUnityRect(), entry.Label, entry.Text);
+		}
+	}
+}
