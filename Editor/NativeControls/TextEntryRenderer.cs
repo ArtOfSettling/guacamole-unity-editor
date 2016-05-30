23e23c2f0ba03a6d04e05471cfa48a66e4765579
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-30 17:25:19 +0100

    Renamed TextStyle to be simply Style

--- Editor/NativeControls/TextEntryRenderer.cs
+++ Editor/NativeControls/TextEntryRenderer.cs
@@ -7,36 +7,36 @@ namespace WellFired.Guacamole.Unity.Editor
 {
 	public class TextEntryRenderer : BaseRenderer
 	{
-		private GUIStyle TextStyle { get; set; }
+		private GUIStyle Style { get; set; }
 
 		public override void Render(UIRect renderRect)
 		{
 			base.Render(renderRect);
 
-			if (TextStyle == null)
-				TextStyle = new GUIStyle();
+			if (Style == null)
+				Style = new GUIStyle();
 
+			Style.focused.background = BackgroundTexture;
+			Style.active.background = BackgroundTexture;
+			Style.hover.background = BackgroundTexture;
+			Style.normal.background = BackgroundTexture;
+			
 			var entry = Control as TextEntry;
 
-			TextStyle.focused.background = BackgroundTexture;
-			TextStyle.active.background = BackgroundTexture;
-			TextStyle.hover.background = BackgroundTexture;
-			TextStyle.normal.background = BackgroundTexture;
+			Style.alignment = UITextAlignExtensions.Combine(entry.HorizontalTextAlign, entry.VerticalTextAlign);
 
-			TextStyle.alignment = UITextAlignExtensions.Combine(entry.HorizontalTextAlign, entry.VerticalTextAlign);
-
-			TextStyle.focused.textColor = entry.TextColor.ToUnityColor();
-			TextStyle.active.textColor = entry.TextColor.ToUnityColor();
-			TextStyle.hover.textColor = entry.TextColor.ToUnityColor();
-			TextStyle.normal.textColor = entry.TextColor.ToUnityColor();
+			Style.focused.textColor = entry.TextColor.ToUnityColor();
+			Style.active.textColor = entry.TextColor.ToUnityColor();
+			Style.hover.textColor = entry.TextColor.ToUnityColor();
+			Style.normal.textColor = entry.TextColor.ToUnityColor();
 
 			var offset = (float)entry.CornerRadius;
 			var smallest = (int)(Mathf.Min(offset, Mathf.Min(renderRect.Width * 0.5f, renderRect.Height * 0.5f)) + 0.5f);
 			smallest = Mathf.Max(smallest, 2);
-			TextStyle.border = new RectOffset(smallest, smallest, smallest, smallest);
-			TextStyle.padding = new RectOffset(smallest, smallest, 0, 0);
+			Style.border = new RectOffset(smallest, smallest, smallest, smallest);
+			Style.padding = new RectOffset(smallest, smallest, 0, 0);
 
-			entry.Text = UnityEditor.EditorGUI.TextField(renderRect.ToUnityRect(), entry.Text, TextStyle);
+			entry.Text = UnityEditor.EditorGUI.TextField(renderRect.ToUnityRect(), entry.Text, Style);
 		}
 	}
 }
