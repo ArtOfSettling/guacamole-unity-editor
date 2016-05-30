42e93e6a72477f1dc0f62cd251679097f9db1b20
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-30 20:38:23 +0100

    Hooked up more bindable properties on the numberentryrenderer.

--- Editor/NativeControls/NumberEntryRenderer.cs
+++ Editor/NativeControls/NumberEntryRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEditor;
+using UnityEngine;
 using WellFired.Guacamole;
 
 [assembly : CustomRenderer(typeof(NumberEntry), typeof(WellFired.Guacamole.Unity.Editor.NumberEntryRenderer))]
@@ -6,12 +7,37 @@ namespace WellFired.Guacamole.Unity.Editor
 {
 	public class NumberEntryRenderer : BaseRenderer
 	{
+		private GUIStyle Style { get; set; }
+
 		public override void Render(UIRect renderRect)
 		{
-			var entry = Control as NumberEntry;
-			var newNumber = UnityEditor.EditorGUI.FloatField(renderRect.ToUnityRect(), entry.Label, entry.Number);
+			base.Render(renderRect);
 
-			if(newNumber != entry.Number)
+			if (Style == null)
+				Style = new GUIStyle();
+
+			Style.focused.background = BackgroundTexture;
+			Style.active.background = BackgroundTexture;
+			Style.hover.background = BackgroundTexture;
+			Style.normal.background = BackgroundTexture;
+
+			var entry = Control as NumberEntry;
+
+			Style.alignment = UITextAlignExtensions.Combine(entry.HorizontalTextAlign, entry.VerticalTextAlign);
+
+			Style.focused.textColor = entry.TextColor.ToUnityColor();
+			Style.active.textColor = entry.TextColor.ToUnityColor();
+			Style.hover.textColor = entry.TextColor.ToUnityColor();
+			Style.normal.textColor = entry.TextColor.ToUnityColor();
+
+			var offset = (float)entry.CornerRadius;
+			var smallest = (int)(Mathf.Min(offset, Mathf.Min(renderRect.Width * 0.5f, renderRect.Height * 0.5f)) + 0.5f);
+			smallest = Mathf.Max(smallest, 2);
+			Style.border = new RectOffset(smallest, smallest, smallest, smallest);
+			Style.padding = new RectOffset(smallest, smallest, 0, 0);
+
+			var newNumber = EditorGUI.FloatField(renderRect.ToUnityRect(), entry.Number, Style);
+			if(Equals(newNumber, entry.Number))
 				entry.Number = newNumber;
 		}
 	}
