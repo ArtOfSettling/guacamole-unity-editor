c1b581cad3ba0bbae7cb3284e0e8bcf70f0f492d
Terry Drever <terrymagnusdrever@wellfired.com>
2016-05-31 11:26:51 +0100

    Tidy up the guacamole window a little.

--- Editor/GuacamoleWindow.cs
+++ Editor/GuacamoleWindow.cs
@@ -8,48 +8,48 @@ namespace WellFired.Guacamole.Unity.Editor
 	public class GuacamoleWindow : EditorWindow, IWindow
 	{
 		[SerializeField]
-		private ApplicationInitializationContextScriptableObject applicationInitializationContextScriptableObject;
+		private ApplicationInitializationContextScriptableObject _applicationInitializationContextScriptableObject;
 		private ApplicationInitializationContextScriptableObject ApplicationInitializationContextScriptableObject
 		{ 
-			get { return applicationInitializationContextScriptableObject; } 
-			set { applicationInitializationContextScriptableObject = value; } 
+			get { return _applicationInitializationContextScriptableObject; } 
+			set { _applicationInitializationContextScriptableObject = value; } 
 		}
 
 		[SerializeField]
-		private WellFired.Guacamole.Window window;
+		private WellFired.Guacamole.Window _window;
 
 		public string Title
 		{
-			get { return this.titleContent.text; }
-			set { this.titleContent.text = value; }
+			get { return titleContent.text; }
+			set { titleContent.text = value; }
 		}
 
 		public UIRect Rect 
 		{
-			get { return this.position.ToUIRect(); }
-			set { this.position = value.ToUnityRect(); }
+			get { return position.ToUIRect(); }
+			set { position = value.ToUnityRect(); }
 		}
 
 		public UISize MinSize 
 		{
-			get { return this.minSize.ToUISize(); }
-			set { this.minSize = value.ToUnityVector2(); }
+			get { return minSize.ToUISize(); }
+			set { minSize = value.ToUnityVector2(); }
 		}
 
 		public UISize MaxSize 
 		{
-			get { return this.maxSize.ToUISize(); }
-			set { this.maxSize = value.ToUnityVector2(); }
+			get { return maxSize.ToUISize(); }
+			set { maxSize = value.ToUnityVector2(); }
 		}
 
 		public WellFired.Guacamole.Window MainContent 
 		{ 
 			get 
 			{
-				if(window == null)
+				if(_window == null)
 					ResetForSomeReason();
 
-				return window;
+				return _window;
 			}
 		}
 
@@ -90,7 +90,7 @@ namespace WellFired.Guacamole.Unity.Editor
 
 			try
 			{
-				MainContent.Render(parentRect: Rect);
+				MainContent.Render(Rect);
 			}
 			catch(Exception e) 
 			{
@@ -103,9 +103,9 @@ namespace WellFired.Guacamole.Unity.Editor
 			NativeRendererHelper.LaunchedAssembly = Assembly.GetExecutingAssembly();
 
 			var contentType = ApplicationInitializationContextScriptableObject.MainContent;
-			window = (Window)contentType.GetConstructor(new Type[] { }).Invoke(new object[] { });
+			_window = (Window)contentType.GetConstructor(new Type[] { }).Invoke(new object[] { });
 
-			if (window == null)
+			if (_window == null)
 				throw new GuacamoleWindowCantBeCreated();
 		}
 	}
